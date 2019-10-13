dfc5b04a3a6c8972ecc5f7b415e53edd39223fbd
williambiao <ziyuan.wang@mail.mcgill.ca>
2019-10-13 17:39:34 -0400

    Update CucumberStepDefinitions.java

--- ca.mcgill.ecse223.quoridor/src/test/java/ca/mcgill/ecse223/quoridor/features/CucumberStepDefinitions.java
+++ ca.mcgill.ecse223.quoridor/src/test/java/ca/mcgill/ecse223/quoridor/features/CucumberStepDefinitions.java
@@ -31,6 +31,7 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
 import ca.mcgill.ecse223.quoridor.controller.Controller;
 import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertTrue;
 
 
 public class CucumberStepDefinitions {
