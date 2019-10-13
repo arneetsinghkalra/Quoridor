76729090af2d0c067f5032845fc47222d6009120
williambiao <ziyuan.wang@mail.mcgill.ca>
2019-10-13 17:32:23 -0400

    combine controllers

--- ca.mcgill.ecse223.quoridor/src/main/java/ca/mcgill/ecse223/quoridor/controller/Controller.java
+++ ca.mcgill.ecse223.quoridor/src/main/java/ca/mcgill/ecse223/quoridor/controller/Controller.java
@@ -73,6 +73,23 @@ public interface Controller {
    */
   public void provideNewUsername(String username);
   
+  
+  /**
+   * @author Sam Perreault
+   * Sets the starting thinking time for the players. Time is accepted as minutes and seconds,
+   * and is converted to milliseconds.
+   * @param minute The number of minutes allowed to each player
+   * @param second The number of seconds allowed to each player
+   */
+  void setPlayerThinkingTime(int minute, int second);
+
+  /**
+   * @author Sam Perreault
+   * Generates the board, and sets the starting position and walls of each player.
+   * In addition, sets white/player 1 as the player to move, and starts counting down
+   * the white player's thinking time.
+   */
+  void initializeBoard();
 }
 
 
