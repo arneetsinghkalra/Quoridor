f830f2093e35290da21cf56037fa8af59e09c144
Ali Tapan <uluc.tapan@mail.mcgill.ca>
2019-12-01 17:42:06 -0500

    More view updates

--- ca.mcgill.ecse223.quoridor/src/main/java/ca/mcgill/ecse223/quoridor/controller/Controller.java
+++ ca.mcgill.ecse223.quoridor/src/main/java/ca/mcgill/ecse223/quoridor/controller/Controller.java
@@ -2204,7 +2204,13 @@ public class Controller {
 		else {
 			currentGame.setGameStatus(GameStatus.Running);
 		}
-		
+		/*
+		int allPositionsSize = currentGame.getPositions().size();
+		int currentPositionIndex = currentGame.getCurrentPosition().getId();
+
+		for(int i = allPositionsSize-1; i>currentPositionIndex; i--) {
+			currentGame.removePosition(currentGame.getPosition(i));
+		}*/
 		
 		return currentGame;
 	}
